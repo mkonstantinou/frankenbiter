f3eb1c37a32fcbd71d580156266f3efc3554d6a1
mkonstantinou <matt.konstantinou@gmail.com>
2017-02-22 22:28:00 -0500

    Initial commits

---
+++ frankenbiter/src/ScriptFragment.py
@@ -0,0 +1,4 @@
+def ScriptFragment:
+  def __init__(self):
+    self.fragmentText = ""
+    self.fragmentClip = None #VideoClip obj
---
+++ frankenbiter/src/VideoClip.py
@@ -0,0 +1,5 @@
+def VideoClip:
+  def __init__(self):
+    self.sourceFile = ""
+    self.startTime = None
+    self.endTime = None
